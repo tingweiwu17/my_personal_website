b28499d877295f4cb0a41d0e5b8ace37721d35ff
Your Name <lydia9017@gmail.com>
2023-04-22 01:09:02 +0800

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/header/header";
 import Nav from "./components/nav/nav";
 import About from "./components/about/about";
 import Experience from "./components/experience/experience";
-import Services from "./components/services/services";
+
 import Protfolio from "./components/protfolio/protfolio";
 import Testimonails from "./components/testimonials/testimonails";
 import Contact from "./components/contact/contact";
